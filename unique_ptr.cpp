77cf8ae23f8d02af0c6ae3b2c3b08d858c61eec0
Whit Armstrong <armstrong.whit@gmail.com>
2022-01-11 23:45:22 -0500

    make example use case more clear

--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -6,7 +6,21 @@ class HasRaw {
   double* raw_dbl;
 public:
   HasRaw(int n): raw_dbl(new double[n]) { cout << "HasRaw::HasRaw" << endl; }
-  ~HasRaw() { cout << "HasRaw::~HasRaw" << endl; }
+  ~HasRaw() {
+    // explicit release required
+    delete[] raw_dbl;
+    cout << "HasRaw::~HasRaw" << endl;
+  }
+};
+
+class HasRaw_up {
+  unique_ptr<double> raw_dbl;
+public:
+  HasRaw_up(int n): raw_dbl(make_unique<double>(n)) { cout << "HasRaw_up::HasRaw_up" << endl; }
+  // explicit release required
+  ~HasRaw_up() {
+    cout << "HasRaw_up::~HasRaw_up" << endl;
+  }
 };
 
 //HasRaw* getRaw() { HasRaw* hrp = new HasRaw(100); }
@@ -14,5 +28,5 @@ unique_ptr<HasRaw> getRaw() { return unique_ptr<HasRaw> (new HasRaw(100)); }
 
 int main() {
   HasRaw hr(10);
-  unique_ptr<HasRaw> hrp = getRaw();
+  HasRaw_up hr_up(100);
 }
