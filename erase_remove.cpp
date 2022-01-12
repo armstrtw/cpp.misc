faff0bde45d988942b10ecb1ed5715e55e17870a
Whit Armstrong <armstrong.whit@gmail.com>
2022-01-11 23:09:22 -0500

    better printing

--- erase_remove.cpp
+++ erase_remove.cpp
@@ -5,6 +5,10 @@
 
 int main() {
   std::vector<int> v {1,2,3,4,5,6};
+  std::cout << "original vector:" << std::endl;
+  std::copy(v.begin(),v.end(),std::ostream_iterator<int>(std::cout, " ")); std::cout << std::endl;
   v.erase(std::remove_if(v.begin(), v.end(), [](int i) { return (i % 2) == 0; }),v.end());
-  std::copy(v.begin(),v.end(),std::ostream_iterator<int>(std::cout, "\n"));
+
+  std::cout << "after remove / erase:" << std::endl;
+  std::copy(v.begin(),v.end(),std::ostream_iterator<int>(std::cout, " "));std::cout << std::endl;
 }
